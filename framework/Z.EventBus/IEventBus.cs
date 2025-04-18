e83958e4c64886e83fa7585172830f1067fc0732
fantasy-ke <2246080525@qq.com>
2025-04-18 18:12:00 +0800

    feat: 添加 Z.EventBus 项目及其基础接口
    添加 Z.EventBus 项目，包含 IEventHandler 和 IEventBus 接口，用于处理事件发布和订阅功能

---
+++ framework/Z.EventBus/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Z.EventBus;
+
+/// <summary>
+/// EventBus 接口
+/// </summary>
+/// <typeparam name="TEvent"></typeparam>
+public interface IEventBus<in TEvent> where TEvent : class
+{
+    ValueTask PublishAsync(TEvent @event);
+}
---
+++ framework/Z.EventBus/IEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Z.EventBus;
+
+/// <summary>
+/// 请求处理者
+/// </summary>
+/// <typeparam name="TEvent"></typeparam>
+public interface IEventHandler<in TEvent> where TEvent : class
+{
+    Task HandleAsync(TEvent @event);
+}   
