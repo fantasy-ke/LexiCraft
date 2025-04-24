ead529a9cc8a8160575e7d6893e95510afa1d44e
fantasy-ke <2246080525@qq.com>
2025-04-24 10:07:38 +0800

    refactor(Z.Local.EventBus): 重构事件总线逻辑并删除无用异常类
    重构了LocalEventBus类，将通道管理逻辑移至新的EventLocalClient类中，以提高代码的可维护性和复用性。同时删除了不再使用的ChannelNullException类，简化了代码结构。

---
+++ framework/Z.Local.EventBus/EventLocalClient.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
+using Z.EventBus;
+using Z.Local.EventBus.Serializer;
+
+namespace Z.Local.EventBus;
+
+/// <summary>
+/// 本地事件Client
+/// </summary>
+/// <param name="logger"></param>
+/// <param name="serviceProvider"></param>
+/// <param name="handlerSerializer"></param>
+public class EventLocalClient(ILogger<EventLocalClient> logger,
+    IServiceProvider serviceProvider, IHandlerSerializer handlerSerializer):IDisposable
+{
+    private readonly ConcurrentDictionary<string, Channel<string>> _channels = new();
+    private readonly CancellationTokenSource _cts = new();
+    
+    
+    /// <summary>
+    /// publish事件指定的Dto Handler
+    /// </summary>
+    /// <param name="eventType"></param>
+    /// <param name="eventData"></param>
+    public async Task PublishAsync(Type eventType, string eventData)
+    {
+        ArgumentNullException.ThrowIfNull(eventType);
+        
+        // 分布式事件总线发布事件，
+        // 且将TEvent的FullName作为事件类型包装，消费者会解析这个类型，然后反序列化为TEvent
+        var channel = CreateChannels(eventType, out var channelName);
+        while (await channel.Writer.WaitToWriteAsync())
+        {
+            var eventDto = new EventEto(channelName,  @eventData);
+            
+            var data = handlerSerializer.SerializeJson(eventDto);
+            
+            await channel.Writer.WriteAsync(data, _cts.Token);
+        }
+    }
+    
+    /// <summary>
+    /// 创建channels
+    /// </summary>
+    /// <param name="eventType"></param>
+    /// <param name="channelName"></param>
+    /// <returns></returns>
+    private Channel<string> CreateChannels(Type eventType, out string channelName)
+    {
+        var attribute = eventType
+            .GetCustomAttributes()
+            .OfType<EventSchemeAttribute>()
+            .FirstOrDefault();
+        
+        channelName = GetChannelName(attribute) ?? eventType.FullName!;
+        
+        var channel = _channels.GetValueOrDefault(channelName!);
+
+        if (channel is not null)
+        {
+            return channel;
+        }
+        
+        channel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions()
+        {
+            SingleReader = attribute?.SingleReader ?? true,
+            SingleWriter =  attribute?.SingleWriter ?? true,
+            AllowSynchronousContinuations =  attribute?.AllowSynchronousContinuations ?? true,
+        });
+        _channels.TryAdd(channelName!, channel);
+
+        logger.LogInformation($"创建channels：{channelName}");
+        return channel;
+    }
+    
+    /// <summary>
+    /// 获取channelName
+    /// </summary>
+    /// <param name="attribute"></param>
+    /// <returns></returns>
+    private static string? GetChannelName(EventSchemeAttribute? attribute)
+    {
+        return string.IsNullOrWhiteSpace(attribute?.EventName) ? null : attribute.EventName;
+    }
+
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    public void Dispose()
+    {
+        _cts.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- framework/Z.Local.EventBus/LocalEventBus.cs
+++ framework/Z.Local.EventBus/LocalEventBus.cs
@@ -3,17 +3,15 @@ using System.Reflection;
 using System.Threading.Channels;
 using Microsoft.Extensions.Logging;
 using Z.EventBus;
+using Z.Local.EventBus.Exceptions;
+using Z.Local.EventBus.Serializer;
 
 namespace Z.Local.EventBus;
 
-public class LocalEventBus<TEvent>(IHandlerSerializer handlerSerializer,
-    ILogger<LocalEventBus<TEvent>> logger)
-    :IEventBus<TEvent>, IDisposable where TEvent : class
+public class LocalEventBus<TEvent>(EventLocalClient eventLocalClient,
+    ILogger<LocalEventBus<TEvent>> logger,IHandlerSerializer handlerSerializer)
+    :IEventBus<TEvent> where TEvent : class
 {
-    
-    private readonly ConcurrentDictionary<string,
-            Channel<string>> _channels = new();
-    private readonly CancellationTokenSource _cts = new();
     
     /// <summary>
     /// publish事件指定的Dto Handler
@@ -21,71 +19,19 @@ public class LocalEventBus<TEvent>(IHandlerSerializer handlerSerializer,
     /// <param name="event"></param>
     public async ValueTask PublishAsync(TEvent @event)
     {
+        // 分布式事件总线发布事件，且将TEvent的FullName作为事件类型包装，消费者会解析这个类型，然后反序列化为TEvent
         ArgumentNullException.ThrowIfNull(@event);
-        
-        // 分布式事件总线发布事件，
-        // 且将TEvent的FullName作为事件类型包装，消费者会解析这个类型，然后反序列化为TEvent
-        var channel = CreateChannels(@event.GetType(), out var channelName);
-        while (await channel.Writer.WaitToWriteAsync())
+
+        try
         {
-            var eventDto = new EventEto(channelName, handlerSerializer.SerializeJson(@event));
-            
-            var data = handlerSerializer.SerializeJson(eventDto);
-            
-            await channel.Writer.WriteAsync(data, _cts.Token);
+            logger.LogInformation("publish事件指定的Dto Handler");
+            await eventLocalClient.PublishAsync(@event.GetType(), handlerSerializer.SerializeJson(@event));
         }
-    }
-
-    /// <summary>
-    /// 创建channels
-    /// </summary>
-    /// <param name="eventType"></param>
-    /// <param name="channelName"></param>
-    /// <returns></returns>
-    private Channel<string> CreateChannels(Type eventType, out string channelName)
-    {
-        var attribute = eventType
-            .GetCustomAttributes()
-            .OfType<EventSchemeAttribute>()
-            .FirstOrDefault();
-        
-        channelName = GetChannelName(attribute) ?? eventType.FullName!;
-        
-        var channel = _channels.GetValueOrDefault(channelName!);
-
-        if (channel is not null)
+        catch (Exception e)
         {
-            return channel;
+            ThrowEventClientException.ThrowException(e.Message);
         }
         
-        channel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions()
-        {
-            SingleReader = attribute?.SingleReader ?? true,
-            SingleWriter =  attribute?.SingleWriter ?? true,
-            AllowSynchronousContinuations =  attribute?.AllowSynchronousContinuations ?? true,
-        });
-        _channels.TryAdd(channelName!, channel);
-
-        logger.LogInformation($"创建channels：{channelName}");
-        return channel;
     }
     
-    /// <summary>
-    /// 获取channelName
-    /// </summary>
-    /// <param name="attribute"></param>
-    /// <returns></returns>
-    private static string? GetChannelName(EventSchemeAttribute? attribute)
-    {
-        return string.IsNullOrWhiteSpace(attribute?.EventName) ? null : attribute.EventName;
-    }
-
-    /// <summary>
-    /// 释放资源
-    /// </summary>
-    public void Dispose()
-    {
-        _cts.Dispose();
-        GC.SuppressFinalize(this);
-    }
 }
