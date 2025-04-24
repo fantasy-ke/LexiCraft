d1a763aed551912ce028e8a4d82c061cb44b3070
fantasy-ke <2246080525@qq.com>
2025-04-24 10:06:49 +0800

    feat(异常处理): 添加EventClientException和ChannelNullException异常类
    添加了两个新的异常类EventClientException和ChannelNullException，用于处理事件客户端异常和通道未实现的情况。这些异常类提供了自定义的异常信息，增强了代码的健壮性和可维护性。

---
+++ framework/Z.Local.EventBus/Exceptions/EventClientException.cs
@@ -0,0 +1,20 @@
+﻿namespace Z.Local.EventBus.Exceptions;
+
+public class EventClientException : Exception
+{
+    public EventClientException()
+    {
+    }
+
+    public EventClientException(string message) : base(message)
+    {
+    }
+}
+
+public static class ThrowEventClientException
+{
+    public static void ThrowException(string str)
+    {
+        throw new EventClientException($"事件处理异常信息{str}");
+    }
+}
