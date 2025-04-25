b21aada6b06eb33e89cad9305eb5691cae9e5e2b
fantasy-ke <2246080525@qq.com>
2025-04-25 15:33:17 +0800

    feat(infrastructure): 添加登录日志数据库迁移
    - 新增登录日志表，包含用户登录相关信息
    - 创建登录日志实体模型和数据库迁移脚本
    - 添加用户、OAuth和设置实体模型注释

---
+++ src/LexiCraft.Infrastructure/Migrations/20250425071729_add-loginlogs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LexiCraft.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addloginlogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "login-log",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    Token = table.Column<string>(type: "text", nullable: true),
+                    LoginTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Origin = table.Column<string>(type: "text", nullable: true),
+                    Ip = table.Column<string>(type: "text", nullable: true),
+                    UserAgent = table.Column<string>(type: "text", nullable: true),
+                    LoginType = table.Column<string>(type: "text", nullable: false),
+                    IsSuccess = table.Column<bool>(type: "boolean", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_login-log", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_login-log_UserId",
+                table: "login-log",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "login-log");
+        }
+    }
+}
