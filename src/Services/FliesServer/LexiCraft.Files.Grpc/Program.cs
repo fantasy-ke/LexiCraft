15782122777c705fa5169c76cc5bf8e9ac12ef9a
fantasy-ke <2246080525@qq.com>
2025-05-22 09:32:25 +0800

    refactor(Files.Grpc):重构项目并添加数据库访问功能- 移除 greet.proto 文件和相关 gRPC 服务 - 新增 WithLexiCraftDbAccess扩展方法以添加数据库访问- 更新 Program.cs 文件 - 删除 ServiceExtensions.cs 文件，将数据库访问相关代码移至 Extentions.cs

--- src/Services/FliesServer/LexiCraft.Files.Grpc/Program.cs
+++ src/Services/FliesServer/LexiCraft.Files.Grpc/Program.cs
@@ -1,5 +1,4 @@
 using BuildingBlocks.Extensions;
-using LexiCraft.Files.Grpc;
 using LexiCraft.Files.Grpc.Data;
 using LexiCraft.Files.Grpc.Services;
 using Microsoft.Extensions.FileProviders;
@@ -9,13 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCodeFirstGrpc();
+// builder.Services.WithScalar(new OpenApiInfo()
+// {
+//     Title = "词汇技艺 Files Api",
+//     Version = "v1",
+//     Description = "词汇技艺相关接口",
+// });
 builder.Services.WithLexiCraftDbAccess(builder.Configuration);
 builder.Services.WithMapster();
 // builder.Services.AddDbContext<FilesDbContext>(opts =>
 //     opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
-
 
 var uploads = Path.Combine(builder.Environment.ContentRootPath, "uploads");
 if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
@@ -30,5 +34,6 @@ app.UseMigration();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 
 app.Run();
